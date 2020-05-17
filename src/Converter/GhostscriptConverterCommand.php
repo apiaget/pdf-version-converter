d4356551c530f533cfa4cdfabb7dffbb50b40be5
apiaget <apiaget@users.noreply.github.com>
2020-05-17 19:56:12 +0200

    Process v5

--- src/Converter/GhostscriptConverterCommand.php
+++ src/Converter/GhostscriptConverterCommand.php
@@ -23,16 +23,19 @@ class GhostscriptConverterCommand
      * @var Filesystem
      */
     protected $baseCommand = 'gs -sDEVICE=pdfwrite -dCompatibilityLevel=%s -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -dColorConversionStrategy=/LeaveColorUnchanged -dEncodeColorImages=false -dEncodeGrayImages=false -dEncodeMonoImages=false -dDownsampleMonoImages=false -dDownsampleGrayImages=false -dDownsampleColorImages=false -dAutoFilterColorImages=false -dAutoFilterGrayImages=false -dColorImageFilter=/FlateEncode -dGrayImageFilter=/FlateEncode  -sOutputFile=%s %s';
-
+    
     public function __construct()
     {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $this->baseCommand = 'gswin64.exe -sDEVICE=pdfwrite -dCompatibilityLevel=%s -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -dColorConversionStrategy=/LeaveColorUnchanged -dEncodeColorImages=false -dEncodeGrayImages=false -dEncodeMonoImages=false -dDownsampleMonoImages=false -dDownsampleGrayImages=false -dDownsampleColorImages=false -dAutoFilterColorImages=false -dAutoFilterGrayImages=false -dColorImageFilter=/FlateEncode -dGrayImageFilter=/FlateEncode  -sOutputFile=%s %s';
+        }
     }
 
     public function run($originalFile, $newFile, $newVersion)
     {
         $command = sprintf($this->baseCommand, $newVersion, $newFile, escapeshellarg($originalFile));
 
-        $process = new Process($command);
+        $process = Process::fromShellCommandline($command);
         $process->run();
 
         if (!$process->isSuccessful()) {
